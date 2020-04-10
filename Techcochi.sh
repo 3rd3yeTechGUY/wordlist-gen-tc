286b0521721b549a55c76ed741be7236ac0b8616
Rixon cochi <61864331+rixon-cochi@users.noreply.github.com>
2020-04-10 13:53:16 +0530

    Update Techcochi.sh

--- Techcochi.sh
+++ Techcochi.sh
@@ -2,13 +2,13 @@
 sleep 2
 figlet TechCochi | lolcat
 Sleep 6
-echo $'\[\e[32m\]PLEASE SUBSCRIBE TECHNICAL COCHI YOUTUBE CHANNEL \[\e[0m\]'
+echo $'\[\e[32m\]PLEASE SUBSCRIBE TECHNICAL COCHI YOUTUBE CHANNEL\[\e[0m\]'
 sleep 4
-echo $'\[\e[33m\]WORDLIST GENERATING BY TECHNICAL COCHI \[\e[0m\]'
+echo $'\[\e[33m\]WORDLIST GENERATING BY TECHNICAL COCHI\[\e[0m\]'
 sleep 4
 
 
-sleep 4 string=( {a..z} )
+string=( {a..z} )
 for x in ${string[@]}; do
 for y in ${string[@]}; do
 for z in ${string[@]}; do
